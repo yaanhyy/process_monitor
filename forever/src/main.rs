a2882f92d22180cbeaa0cf3e2efea7ceb4d0b9cd
hongyuanyang <hongyuanyang@huobi.com>
2020-12-25 23:37:35 +0800

    finish forever not impl set process name

--- forever/src/main.rs
+++ forever/src/main.rs
@@ -1,3 +1,34 @@
+use std::{io, thread, time::Duration, env, convert::AsRef};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let mut input = Default::default();
+    if args.len() == 2 {
+        input = (&args[1]).clone();
+    } else {
+        println!("only need one arg used as pid name");
+        return;
+    }
+    println!("input arg:{}", input);
+
+
+
+
+    while(true) {
+        let mut pid_name = String::new();
+        io::stdin()
+            .read_line(&mut pid_name)
+            .expect("Failed to read line");
+
+        println!("You input process name is: {}", pid_name);
+        thread::spawn(move || {
+            println!("process run:{}", &pid_name);
+           while(true) {
+
+
+           }
+        });
+    }
+
+    println!("exit!");
 }
